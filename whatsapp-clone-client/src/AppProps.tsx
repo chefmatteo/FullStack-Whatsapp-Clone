4c02648b554d4c33030f20f3ce962d53615df18f
Chefmatteo <matthewngbusiness@gmail.com>
2025-09-16 23:42:46 +0800

    Initial commit: Add WhatsApp clone client with React TypeScript setup

---
+++ whatsapp-clone-client/src/AppProps.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface AppProps {
+  name: string;
+}
+
+const App: React.FC<AppProps> = ({name}) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Welcome to the WhatsApp Clone</p>
+    </div>
+  );
+};
+
+export default App;
